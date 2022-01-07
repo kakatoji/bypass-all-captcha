be020617b5c51cdaeb4533d6fc3090a3318397f2
Aang saputra <kakatoji0@gmail.com>
2022-01-07 14:09:41 +0800

    Add files via upload

---
+++ ocr.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "curl.php";
+
+function ocr($key,$data){
+    $ua  =["Host: api.anycaptcha.com","Content-Type: application/json"];
+    $url = "https://api.anycaptcha.com";
+    $data=json_encode([
+        "clientKey" => $key,
+        "task" => [
+            "type"         => "ImageToTextTask",
+            "body"         => base64_encode($data)
+            ],
+        ]);
+    $create =json_decode(curl($url."/createTask",$data,$ua)[1],1);
+    if(!$task = $create["taskId"]){
+        echo "\tanycaptcha ".$create["errorCode"]."\n";return false;
+    }
+    $data = json_encode([
+        "clientKey" => $key,
+        "taskId"    => $create["taskId"]
+        ]);
+    while(true):
+    echo "wait for result....!";
+    $solve=json_decode(curl($url."/getTaskResult",$data,$ua)[1],1);
+    echo "\r                                           \r";
+    if($solve["status"] == "processing"){
+        echo "processing bypass recaptcha";
+        sleep(7);
+        echo "\r                                        \r";
+        continue;}
+        return $solve["solution"]["text"];
+    endwhile;
+}
+
